ebab2cb925df5f057a7b76473deea14aa4ddaf30
Sumiya-Islam-Nafisa <isumiya55@gmail.com>
2025-08-28 21:24:37 +0600

    first commit

---
+++ assets/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",  // adjust to your project structure
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
